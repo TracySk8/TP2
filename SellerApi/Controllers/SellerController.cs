c8aeb651e52016097cfa28c01e160133e47ba5ea
Émile Tracy <emiletracy@hotmail.fr>
2023-04-22 22:51:56 -0400

    Configuration Seller

---
+++ SellerApi/Controllers/SellerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using SellerApi;
+
+namespace SellerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+
+        private HttpClient _httpClient;
+        private JsonSerializerOptions options;
+        public SellerController(SellerDBContext dBContext)
+        {
+            _httpClient = new HttpClient();
+            options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+        }
+        [HttpGet]
+        [Route("GetOk")]
+        public ActionResult Index()
+        {
+            return Ok();
+            //r//eturn View();
+        }
+    }
+}
---
+++ SellerApi/Models/Seller.cs
@@ -0,0 +1,11 @@
+﻿namespace SellerApi.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+
+    }
+}
